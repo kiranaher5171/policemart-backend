065c6df200c5d6cd476ca9599f5966ffcc6d61a8
Kiran Aher <kiran.aher@decimalpointanalytics.com>
2026-04-22 19:35:40 +0530

    Small update

--- config/database.ts
+++ config/database.ts
@@ -1,20 +1,36 @@
 const path = require('path');
 
-module.exports = ({ env }) => ({
-  connection: {
-    client: 'mysql',
+module.exports = ({ env }) => {
+  const client = env('DATABASE_CLIENT', 'sqlite');
+
+  if (client === 'sqlite') {
+    return {
+      connection: {
+        client: 'sqlite',
+        connection: {
+          filename: path.join(process.cwd(), env('DATABASE_FILENAME', '.tmp/data.db')),
+        },
+        useNullAsDefault: true,
+      },
+    };
+  }
+
+  return {
     connection: {
-      host: env('DATABASE_HOST', '127.0.0.1'),
-      port: env.int('DATABASE_PORT', 3306),
-      database: env('DATABASE_NAME', 'strapi'),
-      user: env('DATABASE_USERNAME', 'root'),
-      password: env('DATABASE_PASSWORD', ''),
-      // THIS IS THE MAGIC LINE that forces the MySQL driver to accept Aiven's certificate
-      ssl: env.bool('DATABASE_SSL', false) ? { rejectUnauthorized: false } : false,
+      client: 'mysql',
+      connection: {
+        host: env('DATABASE_HOST', '127.0.0.1'),
+        port: env.int('DATABASE_PORT', 3306),
+        database: env('DATABASE_NAME', 'strapi'),
+        user: env('DATABASE_USERNAME', 'root'),
+        password: env('DATABASE_PASSWORD', ''),
+        ssl: env.bool('DATABASE_SSL', false) ? { rejectUnauthorized: false } : false,
+      },
+      pool: {
+        min: env.int('DATABASE_POOL_MIN', 2),
+        max: env.int('DATABASE_POOL_MAX', 10),
+      },
+      acquireConnectionTimeout: env.int('DATABASE_CONNECTION_TIMEOUT', 60000),
     },
-    pool: {
-      min: 2,
-      max: 10,
-    },
-  },
-});
+  };
+};
