954e29a7b4385ffef4c605d795ebc9b745cb51d6
Kiran Aher <kiran.aher@decimalpointanalytics.com>
2026-04-22 19:10:29 +0530

    update server.ts

--- config/server.ts
+++ config/server.ts
@@ -1,6 +1,7 @@
 module.exports = ({ env }) => ({
   host: env('HOST', '0.0.0.0'),
   port: env.int('PORT', 10000),
+  url: env('PUBLIC_URL', ''), // <--- ADD THIS LINE
   app: {
     keys: env.array('APP_KEYS'),
   },
